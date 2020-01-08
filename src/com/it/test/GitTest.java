52b92703a33e11d52e2ab1500bc8c398cd596758
yangqiang <996042304@qq.com>
2020-01-08 14:22:46 +0800

    这是我的第一个git工程

---
+++ src/com/it/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.it.test;
+
+/**
+ * @author Mr.Yang
+ * @create 2020-01-08-14:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.printf("这是我的第一个git工程");
+    }
+}
